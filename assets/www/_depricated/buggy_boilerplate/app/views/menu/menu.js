bd4415b0bc14ee14519d1b607cc99a5408ced174
steve <ssaanen@gmail.com>
2012-08-20 19:49:16 +0200

    complete refactor of web context based on new boilerplate (old one had nasty persisting bugs)

---
+++ assets/www/_depricated/buggy_boilerplate/app/views/menu/menu.js
@@ -0,0 +1,15 @@
+var menu = function(){
+	var privateProperties = {
+		
+	};
+
+	return {
+		init: function(eventData){
+			//Handle receiving messages from the previous page
+			if(eventData.options.data){
+				console.log(eventData.options.data);
+			}
+		}
+	}
+	
+}();
